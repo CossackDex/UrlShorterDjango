ed76ff611537b093015f89d34770dfc0df661b7e
dexon44 <242589@student.pwr.edu.pl>
2021-06-03 15:36:31 +0200

    admin list fix

--- urlshorter/admin.py
+++ urlshorter/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from .models import UrlShortener
 
-admin.site.register(UrlShortener)
+class UrlShortenerAdmin(admin.ModelAdmin):
+    list_display = ('create_date', 'click_counter', 'long_url', 'short_url')
+admin.site.register(UrlShortener, UrlShortenerAdmin)
 # Register your models here.
